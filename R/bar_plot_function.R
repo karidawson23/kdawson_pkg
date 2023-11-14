7a1305209c54ec173387e3cb6576488c21092f35
Kari Dawson <kari.dawson@selu.edu>
2023-11-14 20:57:27 +0000

    updated stuff

--- R/bar_plot_function.R
+++ R/bar_plot_function.R
@@ -1,3 +1,14 @@
+#' Creates a bar plot
+#' 
+#' Use the length, width, height of an object to calculate its density.
+#' Returns density as a floating point number.
+#' 
+#' @param len Length of the object 
+#' @param width Width of the object 
+#' @param height Height of the object
+#' @return density The density of the object (numeric)
+#'
+#'@export
 #library(ggplot2)
 
 create_bar_plot <- function(data, x_var, y_var, title = "Bar Plot", x_label = NULL, y_label = NULL) {
--- R/stats_function.R
+++ R/stats_function.R
@@ -1,4 +1,17 @@
-perform_anova_and_lm <- function(data, dependent_var, independent_var) {
+#' Omit the na from data set
+#' 
+#' Use the length, width, height of an object to calculate its density.
+#' Returns density as a floating point number.
+#' 
+#' @param len Length of the object 
+#' @param width Width of the object 
+#' @param height Height of the object
+#' @return density The density of the object (numeric)
+#'
+#'@export
+#'
+
+perform_anova_and_lm <- function(data, dependent_var, independent_var){
   # Perform ANOVA
   anova_result <- anova(lm({{dependent_var}} ~ {{independent_var}}, data = data))
   
